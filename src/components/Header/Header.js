9cebac9a763ac8503377d7bad008265b3cbe4dd3
Purvi <purvi09v@gmail.com>
2022-07-09 10:36:09 +0530

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+
+const Header = () => {
+  return (
+    <div className="header">
+        <>
+            <span className="header">🎬 Movies App 🎬</span>
+        </>
+    </div>
+  )
+}
+
+export default Header
